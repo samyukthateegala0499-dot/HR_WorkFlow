3aef3e7d0bb771d3fc9aa229a52cc6f2887a35be
samyuktha <samyukthateegala0499@gmail.com>
2025-12-05 21:10:15 +0530

    sidebar added

---
+++ src/modules/Sidebar.jsx
@@ -0,0 +1,29 @@
+// src/modules/Sidebar.jsx
+import React from 'react'
+
+const items = [
+  { type: 'start', label: 'Start' },
+  { type: 'task', label: 'Task' },
+  { type: 'approval', label: 'Approval' },
+  { type: 'automated', label: 'Automated' },
+  { type: 'end', label: 'End' }
+]
+
+export default function Sidebar(){
+  function onDragStart(e, type){
+    e.dataTransfer.setData('application/reactflow', type)
+    e.dataTransfer.effectAllowed = 'move'
+  }
+
+  return (
+    <div>
+      <h4 className="sectionTitle">Nodes</h4>
+      {items.map(i => (
+        <div key={i.type} className="paletteItem" draggable onDragStart={(e) => onDragStart(e, i.type)}>
+          {i.label}
+        </div>
+      ))}
+      <div style={{ marginTop: 12 }} className="small">Drag nodes onto the canvas</div>
+    </div>
+  )
+}
