90154141d8263c874b5433f59cbee1bf469d69cb
samyuktha <samyukthateegala0499@gmail.com>
2025-12-05 20:09:07 +0530

    node file created

---
+++ src/nodes/StartNode.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Handle, Position } from 'reactflow'
+
+export default function StartNode({ data }) {
+  return (
+    <div style={{ padding:8, borderRadius:8, background:'#e6ffef', border:'1px solid #bfeecf' }}>
+      <strong>{data.title || 'Start'}</strong>
+      <div className="small">{data.title}</div>
+
+      <Handle type="source" position={Position.Bottom} id="a" style={{ background:'#555' }} />
+    </div>
+  )
+}
